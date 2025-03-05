3a903adfe07d3838de0998d834e3eb3f210cfef8
Adrian Edwards <adrian@adriancedwards.com>
2025-03-04 23:08:33 -0500

    move all the uploadthing stuff to one file

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,5 +1,29 @@
-import { uploadRouter } from "../uploadthing-router";
 import { createRouteHandler } from "uploadthing/server";
+// import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const uploadRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+	/**
+       * For full list of options and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 2,
+	
+    },
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;
+
 
 const handler = createRouteHandler({
   router: uploadRouter,
