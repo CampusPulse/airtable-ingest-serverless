0a39ae25b120c11a2ec91a4095cbd506677af19a
Adrian Edwards <adrian@adriancedwards.com>
2025-03-29 18:08:11 -0400

    refactor discord webhooks so that they can be reused

--- api/airtable.ts
+++ api/airtable.ts
@@ -6,6 +6,20 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 );
 const TABLE_NAME = process.env.AIRTABLE_TABLE_NAME || 'FormSubmissions';
 
+
+async function sendDiscordWebhook(endpoint: string, message: string) {
+  // Discord expects a JSON payload that looks like this { content: 'hello world'}
+  if (endpoint != undefined && endpoint != "") {
+    const content = { "content": message }
+    await fetch(endpoint, {
+      body: JSON.stringify(content),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+
+  }
+} 
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -23,23 +37,18 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         fields: { name, email, building, floor, type, description, room, email_optin: email_optin === "true" },
       },
     ]);
-    
+
+    const webhookMessage = `THIS IS A TEST. A new ${type} report has been filed:\n**Building**: ${building}\n**Location Description**: ${room}\n**Description**: ${description}\n\nThis automated message was generated because a report was filed at https://report.campuspulse.app`
+
     if (type == "elevator") {
-      
-      const discordWebhook = process.env.DISCORD_WEBHOOK_URL;
-
-      if (discordWebhook != undefined && discordWebhook != "") {
-        
-        // Discord expects a JSON payload that looks like this { content: 'hello world'}
-        const content = { "content": `${process.env.DISCORD_PING_ROLE ?? ""} THIS IS A TEST. A new elevator report has been filed:\n**Building**: ${building}\n**Location Description**: ${room}\n**Description**: ${description}\n\nThis automated message was generated because a report was filed at https://report.campuspulse.app` }
-        await fetch(discordWebhook, {
-          body: JSON.stringify(content),
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-        })
+      let pingRole = process.env.DISCORD_PING_ROLE ?? ""
+      if (pingRole != "") {
+        pingRole += " "
       }
+      await sendDiscordWebhook(process.env.DISCORD_WEBHOOK_URL ?? "", pingRole + webhookMessage)
+    }   
 
-    }
+    await sendDiscordWebhook(process.env.FIREHOSE_DISCORD_WEBHOOK_URL ?? "", webhookMessage)
     
 
     return res.redirect(302, process.env.REDIRECT_SUCCESS_ROUTE);
